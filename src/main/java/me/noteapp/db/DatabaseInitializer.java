4c58dcfedb068f1e9d35661c42d83778f0b78ab4
Head-BOB <84228915+Head-BOB@users.noreply.github.com>
2025-09-29 15:24:34 +0530

    Db base

--- src/main/java/me/noteapp/db/DatabaseInitializer.java
+++ src/main/java/me/noteapp/db/DatabaseInitializer.java
@@ -1,6 +1,28 @@
 package me.noteapp.db;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DatabaseInitializer {
+
+    public static void initialize() throws SQLException {
+
+        try(Connection conn = DbConnectionManager.getConnection();
+            Statement stmt = conn.createStatement()) {
+
+            //db login here for creating tables
+
+
+        } catch (SQLException e) {
+
+            System.err.println("Initialization error" + e.getMessage());
+
+        }
+
+        System.out.println("DB init complete.");
+
+    }
 
     //database table creation etc ivde
 }
--- src/main/java/me/noteapp/db/DbConnectionManager.java
+++ src/main/java/me/noteapp/db/DbConnectionManager.java
@@ -1,6 +1,41 @@
 package me.noteapp.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DbConnectionManager {
 
     //manages database connections
+
+    private static final String DB_URL = "jdbc:sqlite:notesapp.db";
+
+
+    public static Connection getConnection() throws SQLException {
+
+
+
+            // CONNECtion logic ehere
+
+
+
+
+        return DriverManager.getConnection(DB_URL);
+    }
+
+
+    public static void closeConnection(Connection conn) {
+
+        if(conn != null) {
+
+            try{
+                conn.close();
+            } catch (SQLException e) {
+
+                System.err.println("Error closing connection: " + e.getMessage());
+
+            }
+        }
+
+    }
 }
