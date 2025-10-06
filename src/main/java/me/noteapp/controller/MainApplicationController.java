b7644726498ae955ca2da2e0125506d76a8892be
neha525 <nehajose860@gmail.com>
2025-10-06 21:55:19 +0530

    updated pane

--- src/main/java/me/noteapp/controller/MainApplicationController.java
+++ src/main/java/me/noteapp/controller/MainApplicationController.java
@@ -1,9 +1,46 @@
 package me.noteapp.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 public class MainApplicationController {
+    private double xOffset = 0;
+    private double yOffset = 0;
 
+    @FXML
+    public void onMousePressed(MouseEvent event) {
+
+        xOffset = event.getSceneX();
+        yOffset = event.getSceneY();
+
+    }
+
+    @FXML
+    public void onMouseDragged(MouseEvent event) {
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        stage.setX(event.getScreenX() - xOffset);
+        stage.setY(event.getScreenY() - yOffset);
+
+    }
+    public void handleMinimize(ActionEvent event){
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setIconified(true);
+
+    }
+
+    @FXML
+    public void handleClose(ActionEvent event){
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
+
+    }
     @FXML
     public void initialize() {
 
