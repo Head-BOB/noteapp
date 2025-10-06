2c32c8091b07e9e08ff23089713ff5ff85e0dccd
Head-BOB <84228915+Head-BOB@users.noreply.github.com>
2025-10-06 19:40:25 +0530

    Donot merge!

--- src/main/java/me/noteapp/service/NoteService.java
+++ src/main/java/me/noteapp/service/NoteService.java
@@ -1,6 +1,15 @@
 package me.noteapp.service;
 
+import me.noteapp.db.DbConnectionManager;
+import me.noteapp.model.Note;
+
+import java.sql.SQLException;
+
 public class NoteService {
 
-    //all note operation ehre create note,getnote etc..
+    public Note createNote(int userId, String title, String content) throws SQLException {
+
+        String sql = "INSERT INTO Notes (user_id, title, content, created_at, updated_at, is_archived) VALUES (?, ?, ?, ?, ?, ?)";
+        //try(Connection conn = DbConnectionManager.getConnection())
+    }
 }
